622fa2e1bf98971629e770aeb5c56947699c3df6
Eric Cheng <ericcheng986@gmail.com>
2019-12-25 12:41:16 +0800

    15122

---
+++ 15122-notes.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Downloads all 15122 notes
+wget https://www.cs.cmu.edu/~15122/handouts/01-contracts.pdf
+wget https://www.cs.cmu.edu/~15122/handouts/02-ints.pdf
+wget https://www.cs.cmu.edu/~15122/handouts/03-arrays.pdf
+wget https://www.cs.cmu.edu/~15122/handouts/04-linesearch.pdf
+wget https://www.cs.cmu.edu/~15122/handouts/05-bigo.pdf
+wget https://www.cs.cmu.edu/~15122/handouts/06-binsearch.pdf
+wget https://www.cs.cmu.edu/~15122/handouts/07-quicksort.pdf
+wget https://www.cs.cmu.edu/~15122/handouts/08-datastruct.pdf
+wget https://www.cs.cmu.edu/~15122/handouts/09-stackqueue.pdf
+wget https://www.cs.cmu.edu/~15122/handouts/10-linkedlist.pdf
+wget https://www.cs.cmu.edu/~15122/handouts/11-ubarrays.pdf
+wget https://www.cs.cmu.edu/~15122/handouts/12-hashing.pdf
+wget https://www.cs.cmu.edu/~15122/handouts/13-hdictist.pdf
+wget https://www.cs.cmu.edu/~15122/handouts/14-generic.pdf
+wget https://www.cs.cmu.edu/~15122/handouts/15-bst.pdf
+wget https://www.cs.cmu.edu/~15122/handouts/16-avl.pdf
+wget https://www.cs.cmu.edu/~15122/handouts/17-pq.pdf
+wget https://www.cs.cmu.edu/~15122/handouts/18-resinv.pdf
+wget https://www.cs.cmu.edu/~15122/handouts/19-cstructure.pdf
+wget https://www.cs.cmu.edu/~15122/handouts/20-mem.pdf
+wget https://www.cs.cmu.edu/~15122/handouts/21-types.pdf
+wget https://www.cs.cmu.edu/~15122/handouts/22-c0vm.pdf
+wget https://www.cs.cmu.edu/~15122/handouts/23-graphs.pdf
+wget https://www.cs.cmu.edu/~15122/handouts/24-graphsearch.pdf
+wget https://www.cs.cmu.edu/~15122/handouts/25-spanning.pdf
+wget https://www.cs.cmu.edu/~15122/handouts/26-unionfind.pdf
